2f1d5f78d2c122341bdf78f13b0b9fa3d43426ae
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2025-01-15 11:36:51 -0500

    Cloned project, added the Unity UI package, and some project organization in the form of a root namespace and subfolders

---
+++ CarScraper/Assets/Scripts/Vehicle/CarControls.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CarScraper.Vehicles
+{
+    public class CarControls : MonoBehaviour
+    {
+
+        public Rigidbody rigid;
+        public WheelCollider wheel_L_F, wheel_L_B, wheel_R_F, wheel_R_B;
+        public float drivespeed, steerspeed;
+        float horizontalInput, verticalInput;
+
+        void Update()
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+        }
+
+        void FixedUpdate()
+        {
+            float motor = Input.GetAxis("Vertical") * drivespeed;
+            wheel_L_F.motorTorque = motor;
+            wheel_L_B.motorTorque = motor;
+            wheel_R_F.motorTorque = motor;
+            wheel_R_B.motorTorque = motor;
+            wheel_L_F.steerAngle = steerspeed * horizontalInput;
+            wheel_L_B.steerAngle = steerspeed * horizontalInput;
+        }
+    }
+}
