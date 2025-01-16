fd613df79e9aa85a2acf1af8273cb6bed4fd8d7c
Leo Schindler-Gerendasi <ls1539@rit.edu>
2025-01-16 16:33:24 -0500

    Added basic tether mechanics
    Pretty much just locks the car's movement once it's far enough away from a tether point. It's currently only visualized by a gizmo, but I'll do more with it soon enough

---
+++ CarScraper/Assets/Scripts/Vehicle/Tether.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace CarScraper
+{
+    public class Tether : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 tetherPoint;
+        [SerializeField]
+        private Rigidbody carRB;
+        [SerializeField]
+        private float maxDistance;
+        private Vector3 dist;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            dist = tetherPoint - carRB.transform.position;
+            if (dist.magnitude > maxDistance)
+            {
+                carRB.transform.position = tetherPoint - dist.normalized * maxDistance;
+                carRB.AddForce(dist * 1000);
+                carRB.AddTorque(Vector3.up * 5);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawRay(tetherPoint, -dist);
+        }
+    }
+}
