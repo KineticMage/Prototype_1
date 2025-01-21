263078d3f36cd8b05713a61725f0880273e6ad61
Ayushi Parikh <mailayushi15@gmail.com>
2025-01-20 22:24:52 -0500

    Fixing speed boost and adding damping

--- CarScraper/Assets/Scripts/Environment/SpeedBoost.cs
+++ CarScraper/Assets/Scripts/Environment/SpeedBoost.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using CarScraper.Vehicles;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
@@ -6,26 +7,65 @@ namespace CarScraper
 {
     public class SpeedBoost : MonoBehaviour
     {
-        [SerializeField] float boostMultiplier = 2f; // Boost factor
+        [Header("SpeedBoost")]
+        [SerializeField] float boostForce = 1000f; // Force to apply
         [SerializeField] float boostDuration = 3f;  // How long the boost lasts
-        [SerializeField] CarControls carControls;
+
+        [Header("LinearDamping")]
+        [SerializeField] float maxlinearDamping = 5f;
+        [SerializeField] float dampingDuration = 1f;
+        private float originallinearDamping = 0f;
+
+        private Rigidbody carRb;
+
+        private void Awake()
+        {
+            carRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+            originallinearDamping = carRb.linearDamping;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Wheels")
             {
-                if (carControls != null)
+                if (carRb != null)
                 {
-                    Debug.Log("Speed Boost Activated!");
-                    carControls.ApplySpeedBoost(boostMultiplier, boostDuration);
+                    StartCoroutine(ApplySpeedBoost(carRb, boostDuration));
                 }
-                //Rigidbody carRigidbody = other.GetComponent<Rigidbody>();
-                //if (carRigidbody != null)
-                //{
-                //    // Apply a forward force to the car's Rigidbody
-                //    carRigidbody.AddForce(other.transform.forward * boostForce, ForceMode.Impulse);
-
-                //}
             }
+        }
+
+        private IEnumerator ApplySpeedBoost(Rigidbody rb, float boostDuration)
+        {
+            float elapsed = 0f;
+
+            while (elapsed < boostDuration)
+            {
+                Debug.Log("Speed Boost Activated!");
+                rb.AddForce(transform.forward * boostForce * Time.deltaTime, ForceMode.Acceleration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+            Debug.Log("Stopped");
+
+            //Adding linear damping to ease down the vehicle stopping
+            yield return StartCoroutine(GraduallyReducelinearDamping(rb, maxlinearDamping, originallinearDamping, dampingDuration));
+        }
+
+        private IEnumerator GraduallyReducelinearDamping(Rigidbody rb, float maxlinearDamping, float originallinearDamping, float dampingDuration)
+        {
+            rb.linearDamping = maxlinearDamping;
+            float elapsed = 0f;
+
+            while (elapsed < dampingDuration)
+            {
+                //rb.linearDamping = Mathf.Lerp(maxlinearDamping, originallinearDamping, 1f - Mathf.Exp(-elapsed / dampingDuration));
+                rb.linearDamping = Mathf.Lerp(maxlinearDamping, originallinearDamping, elapsed / dampingDuration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            rb.linearDamping = originallinearDamping; 
         }
     }
 }
